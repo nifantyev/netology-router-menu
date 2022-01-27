0f4b08178cf872b7aeeec4ab8b447ff823ddb591
Евгений Нифантьев <nifantyev@gmail.com>
2022-01-27 11:04:26 +0300

    Реализовал компонент Menu

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,3 +1,41 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
 export default function Menu() {
-  return null;
+  return (
+    <nav className="menu">
+      <NavLink
+        to="/"
+        className={({ isActive }) =>
+          'menu__item' + (isActive ? ' menu__item-active' : '')
+        }
+      >
+        Главная
+      </NavLink>
+      <NavLink
+        to="/drift"
+        className={({ isActive }) =>
+          'menu__item' + (isActive ? ' menu__item-active' : '')
+        }
+      >
+        Дрифт-такси
+      </NavLink>
+      <NavLink
+        to="/timeattack"
+        className={({ isActive }) =>
+          'menu__item' + (isActive ? ' menu__item-active' : '')
+        }
+      >
+        Time Attack
+      </NavLink>
+      <NavLink
+        to="/forza"
+        className={({ isActive }) =>
+          'menu__item' + (isActive ? ' menu__item-active' : '')
+        }
+      >
+        Forza Karting
+      </NavLink>
+    </nav>
+  );
 }
